07a141b1649c806fe9e186d023431ecaccb3ab85
PiKaJoe <50702649+jojoextra@users.noreply.github.com>
2026-04-09 11:45:15 +0700

    display quote

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ function newQuote() {
 
 var quotes = ["Hello World", "You can do it", "Never give up"];
 var random = Math.floor(Math.random() * quotes.length);
+document.getElementById("quote").innerText = quotes[random];
